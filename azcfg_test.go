281bd9265ec98380ff9831aa6ef21d05964ed89b
Karl Wallenius <karl.wallenius@gmail.com>
2023-03-24 00:37:27 +0100

    Add secrets (string map) to writeErrorMessage to to be able get empty required

--- azcfg_test.go
+++ azcfg_test.go
@@ -121,9 +121,9 @@ func TestParseRequired(t *testing.T) {
 		wantErr error
 	}{
 		{
-			name:    "required",
+			name:    "1 required",
 			input:   StructWithRequired{},
-			wantErr: fmt.Errorf("secret: %s is required", "empty"),
+			wantErr: fmt.Errorf("secrets: %s and %s are required", "empty", "empty-float64"),
 		},
 	}
 
@@ -239,6 +239,90 @@ func TestSplitTrim(t *testing.T) {
 	}
 }
 
+func TestRequiredErrorMessage(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			secrets  map[string]string
+			required []string
+		}
+		want string
+	}{
+		{
+			name: "no required",
+			input: struct {
+				secrets  map[string]string
+				required []string
+			}{
+				secrets:  map[string]string{},
+				required: []string{},
+			},
+			want: "",
+		},
+		{
+			name: "1 required",
+			input: struct {
+				secrets  map[string]string
+				required []string
+			}{
+				secrets: map[string]string{
+					"secret1": "",
+				},
+				required: []string{
+					"secret1",
+				},
+			},
+			want: "secret: secret1 is required",
+		},
+		{
+			name: "2 required",
+			input: struct {
+				secrets  map[string]string
+				required []string
+			}{
+				secrets: map[string]string{
+					"secret1": "",
+					"secret2": "",
+				},
+				required: []string{
+					"secret1",
+					"secret2",
+				},
+			},
+			want: "secrets: secret1 and secret2 are required",
+		},
+		{
+			name: "3 required",
+			input: struct {
+				secrets  map[string]string
+				required []string
+			}{
+				secrets: map[string]string{
+					"secret1": "",
+					"secret2": "",
+					"secret3": "",
+				},
+				required: []string{
+					"secret1",
+					"secret2",
+					"secret3",
+				},
+			},
+			want: "secrets: secret1, secret2 and secret3 are required",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := requiredErrorMessage(test.input.secrets, test.input.required)
+
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("requiredErrorMessage(%+v, %+v) = unexpected result, (-want, +got)\n%s\n", test.input.secrets, test.input.secrets, diff)
+			}
+		})
+	}
+}
+
 type Struct struct {
 	String                  string  `secret:"string"`
 	StringPtr               *string `secret:"string-ptr"`
@@ -274,8 +358,14 @@ type NestedNestedStruct struct {
 }
 
 type StructWithRequired struct {
-	String string `secret:"string"`
-	Empty  string `secret:"empty,required"`
+	String                   string `secret:"string"`
+	Empty                    string `secret:"empty,required"`
+	NestedStructWithRequired NestedStructWithRequired
+}
+
+type NestedStructWithRequired struct {
+	Int     int     `secret:"number"`
+	Float64 float64 `secret:"empty-float64,required"`
 }
 
 type mockKeyVaultClient struct {

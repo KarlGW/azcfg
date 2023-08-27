d691e1762ac5fadde09f7500d655296869733812
Karl Wallenius <karl.wallenius@gmail.com>
2023-08-27 07:24:30 +0200

    feat: add an adapter struct to handle other credential types

--- internal/auth/credential.go
+++ internal/auth/credential.go
@@ -5,13 +5,29 @@ import (
 	"time"
 )
 
-// Token contains the access token when it expires.
+var (
+	// Scopes contains the target scopes for getting Key Vault secrets.
+	Scopes = []string{"https://vault.azure.net/.default"}
+)
+
+// Token contains the access token and when it expires.
 type Token struct {
 	AccessToken string
-	Expires     time.Time
+	ExpiresOn   time.Time
 }
 
 // Credential is the interface that wraps around method Token.
 type Credential interface {
 	Token(ctx context.Context) (Token, error)
+}
+
+// AdaptorCredential is used when other credentials are provided than
+// the built-in one.
+type AdaptorCredential struct {
+	TokenFunc func(ctx context.Context) (Token, error)
+}
+
+// Token retreives access token.
+func (a *AdaptorCredential) Token(ctx context.Context) (Token, error) {
+	return a.TokenFunc(ctx)
 }

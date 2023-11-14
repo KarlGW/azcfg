0bc5011598446cd801e4f2bb95f5eb8aae938434
Karl Wallenius <karl.wallenius@gmail.com>
2023-11-14 23:33:23 +0100

    feat: add methods Scope and SetScope to Credential interface

--- auth/auth.go
+++ auth/auth.go
@@ -11,7 +11,20 @@ type Token struct {
 	ExpiresOn   time.Time
 }
 
-// Credential is the interface that wraps around method Token.
+// Credential is the interface that wraps around method Token, Scope
+// and SetScope.
 type Credential interface {
 	Token(ctx context.Context) (Token, error)
+	Scope() Scope
+	SetScope(scope Scope)
 }
+
+// Scope represents a scope for a token.
+type Scope string
+
+const (
+	// ScopeKeyVault is the scope needed for tokens to be used with Azure Key Vault.
+	ScopeKeyVault Scope = "https://vault.azure.net/.default"
+	// ScopeAppConfig is the scope needed for tokens to be used with Azure App Config.
+	ScopeAppConfig Scope = "https://azconfig.io/.default"
+)

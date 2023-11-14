c7a79bf6bdd4a4a4642d81ad4ad144ee3908dab0
Karl Wallenius <karl.wallenius@gmail.com>
2023-11-14 22:17:25 +0100

    feat: add stub for settings

--- stub/stub.go
+++ stub/stub.go
@@ -1,6 +1,9 @@
 package stub
 
-import "github.com/KarlGW/azcfg/internal/secret"
+import (
+	"github.com/KarlGW/azcfg/internal/secret"
+	"github.com/KarlGW/azcfg/internal/setting"
+)
 
 // SecretClient satisfies the secretClient interface in package azcfg. Purpose is
 // to be used as a stub for secret retrieval.
@@ -9,7 +12,7 @@ type SecretClient struct {
 	err     error
 }
 
-// NewSecretClient creates a new SecretClient. Argument the map[string] secrets
+// NewSecretClient creates a new SecretClient. Argument the map[string]string secrets
 // should be key-value pairs of secret names and values. Argument
 // error is to set if method calls should return an error.
 func NewSecretClient(secrets map[string]string, err error) SecretClient {
@@ -26,9 +29,40 @@ func NewSecretClient(secrets map[string]string, err error) SecretClient {
 }
 
 // Get secrets set to the stub client.
-func (c SecretClient) Get(names ...string) (map[string]secret.Secret, error) {
+func (c SecretClient) GetSecrets(names []string, options ...secret.Option) (map[string]secret.Secret, error) {
 	if c.err != nil {
 		return nil, c.err
 	}
 	return c.secrets, nil
+}
+
+// SettingClient satisfies the settingClient interface in package azcfg. Purpose is
+// to be used as a stub for setting retrieval.
+type SettingClient struct {
+	settings map[string]setting.Setting
+	err      error
+}
+
+// NewSettingClient creates a new SettingClient. Argument the map[string]string settings
+// should be key-value pairs of setting keys and values. Argument
+// error is to set if method calls should return an error.
+func NewSettingClient(settings map[string]string, err error) SettingClient {
+	s := make(map[string]setting.Setting, len(settings))
+	for k, v := range settings {
+		s[k] = setting.Setting{
+			Value: v,
+		}
+	}
+	return SettingClient{
+		settings: s,
+		err:      err,
+	}
+}
+
+// Get settings set to the stub client.
+func (c SettingClient) GetSettings(keys []string, options ...setting.Option) (map[string]setting.Setting, error) {
+	if c.err != nil {
+		return nil, c.err
+	}
+	return c.settings, nil
 }
--- stub/stub_test.go
+++ stub/stub_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/KarlGW/azcfg/internal/secret"
+	"github.com/KarlGW/azcfg/internal/setting"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
@@ -26,7 +27,7 @@ func TestNewSecretClient(t *testing.T) {
 	})
 }
 
-func TestSecretClient_Get(t *testing.T) {
+func TestSecretClient_GetSecrets(t *testing.T) {
 	var tests = []struct {
 		name  string
 		input struct {
@@ -78,17 +79,102 @@ func TestSecretClient_Get(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			cl := NewSecretClient(test.input.secrets, test.input.err)
-			got, gotErr := cl.Get(test.input.names...)
+			got, gotErr := cl.GetSecrets(test.input.names)
 
 			if diff := cmp.Diff(test.want, got); diff != "" {
-				t.Errorf("Get() = unexpected result (-want +got)\n%s\n", diff)
+				t.Errorf("GetSecrets() = unexpected result (-want +got)\n%s\n", diff)
 			}
 
 			if diff := cmp.Diff(test.wantErr, gotErr, cmpopts.EquateErrors()); diff != "" {
-				t.Errorf("Get() = unexpected error (-want +got)\n%s\b", diff)
+				t.Errorf("GetSecrets() = unexpected error (-want +got)\n%s\b", diff)
 			}
 		})
 	}
 }
 
-var errGetSecrets = errors.New("err")
+func TestNewSettingClient(t *testing.T) {
+	t.Run("NewSettingClient", func(t *testing.T) {
+		want := SettingClient{
+			settings: map[string]setting.Setting{},
+			err:      errors.New("test"),
+		}
+
+		got := NewSettingClient(map[string]string{
+			"setting": "value",
+		}, errors.New("test"))
+
+		if diff := cmp.Diff(want, got, cmp.AllowUnexported(SettingClient{}), cmpopts.IgnoreUnexported(SettingClient{})); diff != "" {
+			t.Errorf("NewSettingCLient() = unexpected result (-want +got)\n%s\n", diff)
+		}
+	})
+}
+
+func TestSettingClient_GetSettings(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			keys     []string
+			settings map[string]string
+			err      error
+		}
+		want    map[string]setting.Setting
+		wantErr error
+	}{
+		{
+			name: "Get settings",
+			input: struct {
+				keys     []string
+				settings map[string]string
+				err      error
+			}{
+				keys: []string{"setting"},
+				settings: map[string]string{
+					"setting": "value",
+				},
+				err: nil,
+			},
+			want: map[string]setting.Setting{
+				"setting": {
+					Value: "value",
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Get error",
+			input: struct {
+				keys     []string
+				settings map[string]string
+				err      error
+			}{
+				keys: []string{"setting"},
+				settings: map[string]string{
+					"setting": "value",
+				},
+				err: errGetSettings,
+			},
+			want:    nil,
+			wantErr: errGetSettings,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cl := NewSettingClient(test.input.settings, test.input.err)
+			got, gotErr := cl.GetSettings(test.input.keys)
+
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("GetSettings() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if diff := cmp.Diff(test.wantErr, gotErr, cmpopts.EquateErrors()); diff != "" {
+				t.Errorf("GetSettings() = unexpected result (-want +got)\n%s\n", diff)
+			}
+		})
+	}
+}
+
+var (
+	errGetSecrets  = errors.New("err")
+	errGetSettings = errors.New("err")
+)

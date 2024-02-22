4b525482467e7b1d2f7aa81af39190678797d0e5
Karl Wallenius <karl.wallenius@gmail.com>
2024-02-23 00:48:10 +0100

    refactor: add environment variable names to contants

---
+++ environment.go
@@ -0,0 +1,12 @@
+package azcfg
+
+const (
+	azcfgTenantID              = "AZCFG_TENANT_ID"
+	azcfgClientID              = "AZCFG_CLIENT_ID"
+	azcfgClientSecret          = "AZCFG_CLIENT_SECRET"
+	azcfgCertificate           = "AZCFG_CERTIFICATE"
+	azcfgCertificatePath       = "AZCFG_CERTIFICATE_PATH"
+	azcfgKeyVaultName          = "AZCFG_KEYVAULT_NAME"
+	azcfgAppConfigurationName  = "AZCFG_APPCONFIGURATION_NAME"
+	azcfgAppConfigurationLabel = "AZCFG_APPCONFIGURATION_LABEL"
+)

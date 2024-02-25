da2bd2ec2f2a190b5b01bd00739a3323f18d5cde
Karl Wallenius <karl.wallenius@gmail.com>
2024-02-25 11:19:55 +0100

    chore: create constant for all environment variable strings in file environment.go

--- environment.go
+++ environment.go
@@ -1,12 +1,20 @@
 package azcfg
 
 const (
-	azcfgTenantID              = "AZCFG_TENANT_ID"
-	azcfgClientID              = "AZCFG_CLIENT_ID"
-	azcfgClientSecret          = "AZCFG_CLIENT_SECRET"
-	azcfgCertificate           = "AZCFG_CERTIFICATE"
-	azcfgCertificatePath       = "AZCFG_CERTIFICATE_PATH"
-	azcfgKeyVaultName          = "AZCFG_KEYVAULT_NAME"
-	azcfgAppConfigurationName  = "AZCFG_APPCONFIGURATION_NAME"
+	// azcfgTenantID is the environment variable for the tenant ID.
+	azcfgTenantID = "AZCFG_TENANT_ID"
+	// azcfgClientID is the environment variable for the client ID.
+	azcfgClientID = "AZCFG_CLIENT_ID"
+	// azcfgClientSecret is the environment variable for the client secret.
+	azcfgClientSecret = "AZCFG_CLIENT_SECRET"
+	// azcfgCertificate is the environment variable for the certificate.
+	azcfgCertificate = "AZCFG_CERTIFICATE"
+	// azcfgCertificatePath is the environment variable for the certificate path.
+	azcfgCertificatePath = "AZCFG_CERTIFICATE_PATH"
+	// azcfgKeyVaultName is the environment variable for the Key Vault name.
+	azcfgKeyVaultName = "AZCFG_KEYVAULT_NAME"
+	// azcfgAppConfigurationName is the environment variable for the App Configuration name.
+	azcfgAppConfigurationName = "AZCFG_APPCONFIGURATION_NAME"
+	// azcfgAppConfigurationLabel is the environment variable for the App Configuration label.
 	azcfgAppConfigurationLabel = "AZCFG_APPCONFIGURATION_LABEL"
 )

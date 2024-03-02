6168cb07504235c14ff57430f685b6fe67dd9131
Karl Wallenius <karl.wallenius@gmail.com>
2024-03-02 01:23:49 +0100

    fix: env var names for certificate

--- environment.go
+++ environment.go
@@ -7,10 +7,10 @@ const (
 	azcfgClientID = "AZCFG_CLIENT_ID"
 	// azcfgClientSecret is the environment variable for the client secret.
 	azcfgClientSecret = "AZCFG_CLIENT_SECRET"
-	// azcfgCertificate is the environment variable for the certificate.
-	azcfgCertificate = "AZCFG_CERTIFICATE"
-	// azcfgCertificatePath is the environment variable for the certificate path.
-	azcfgCertificatePath = "AZCFG_CERTIFICATE_PATH"
+	// azcfgCertificate is the environment variable for the client certificate.
+	azcfgCertificate = "AZCFG_CLIENT_CERTIFICATE"
+	// azcfgCertificatePath is the environment variable for the path to the client certificate.
+	azcfgCertificatePath = "AZCFG_CLIENT_CERTIFICATE_PATH"
 	// azcfgKeyVaultName is the environment variable for the Key Vault name.
 	azcfgKeyVaultName = "AZCFG_KEYVAULT_NAME"
 	// azcfgAppConfigurationName is the environment variable for the App Configuration name.
